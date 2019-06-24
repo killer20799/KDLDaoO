011802995d721aacfc8bc9448dfbf9a4a83cbcbe
ginle20799 <leminhnhat20799@gmail.com>
2019-06-24 14:56:28 +0700

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,39 @@
 $(document).ready(function () {
-	// Viết Script ở đây!
+	sliderImage();
+	otherNewClient();
+	bannerSlider();
 });
+function sliderImage() {
+	var swiper = new Swiper('.slider-image .swiper-container', {
+		speed: 2000,
+		navigation: {
+		  nextEl: '.slider-image .nav-next',
+		  prevEl: '.slider-image .nav-prev',
+		},
+	  });
+  
+}
+function bannerSlider() {
+	var swiper = new Swiper('.home-banner .swiper-container', {
+		speed: 2000,
+		pagination: {
+			el: '.home-banner .swiper-pagination',
+		  },
+	  });
+  
+}
+function otherNewClient() {
+	var swiper = new Swiper('.giaitri .swiper-container', {
+		spaceBetween: 20,
+		slidesPerView: 3,
+		speed: 2000,
+		breakpoints: {
+			1024: {
+				slidesPerView: 2,
+			},
+			576: {
+				slidesPerView: 1,
+			}
+		}
+	})
+}
