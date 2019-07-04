892592196f56415053b2447ab41d300c854e99e1
ginle20799 <leminhnhat20799@gmail.com>
2019-07-04 13:11:42 +0700

    no message

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
+	moveSearchBox();
+	moveNav();
+	toggleMenu();
 	sliderImage();
 	otherNewClient();
 	bannerSlider();
 	roomTypeSlide();
 	homeBannerSlider();
+	moveContactInfo();
+	moveLogo();
+	toggleSearchBox();
+	
 });
 function sliderImage() {
 	var swiper = new Swiper('.slider-image .swiper-container', {
@@ -14,6 +21,67 @@ function sliderImage() {
 		},
 	  });
   
+}
+
+function toggleMenu() {
+	let toggleMenu = $('header .toggle-menu')
+	let backdrop = $('header .backdrop')
+	let bodyOverFlow = $('body')
+	let mobileHeader = $('.mobile-header')
+	toggleMenu.on('click', function () {
+		toggleMenu.toggleClass('active')
+		backdrop.toggleClass('active')
+		bodyOverFlow.toggleClass('active')
+		mobileHeader.toggleClass('active')
+	})
+}
+function moveSearchBox() {
+	let moveContactInfo = new MappingListener({
+		selector: 'header .searchbox',
+		mobileWrapper: '.mobile-tab',
+		mobileMethod: 'appendTo',
+		desktopWrapper: '.header-info .desktop-header-1',
+		desktopMethod: 'prependTo',
+		breakpoint: 992,
+	}).watch()
+}
+function moveNav() {
+	let moveContactInfo = new MappingListener({
+		selector: 'header .desktop-header-2',
+		mobileWrapper: '.mobile-tab',
+		mobileMethod: 'appendTo',
+		desktopWrapper: '.header-nav .container',
+		desktopMethod: 'prependTo',
+		breakpoint: 992,
+	}).watch()
+}
+function moveContactInfo() {
+	let moveContactInfo = new MappingListener({
+		selector: 'header .desktop-header-1',
+		mobileWrapper: '.mobile-tab',
+		mobileMethod: 'appendTo',
+		desktopWrapper: '.header-info .container',
+		desktopMethod: 'prependTo',
+		breakpoint: 992,
+	}).watch()
+}
+function toggleSearchBox() {
+	let searchIcon = $('.searchbutton')
+	let searchBox = $('.search-box')
+	searchIcon.on('click', function () {
+		searchBox.toggleClass('active')
+	})
+
+}
+function moveLogo() {
+	let moveContactInfo = new MappingListener({
+		selector: 'header .logo',
+		mobileWrapper: '.mobile-header',
+		mobileMethod: 'appendTo',
+		desktopWrapper: '.desktop-header-2',
+		desktopMethod: 'prependTo',
+		breakpoint: 992,
+	}).watch()
 }
 function bannerSlider() {
 	var swiper = new Swiper('.banner .swiper-container', {
@@ -36,7 +104,7 @@ function otherNewClient() {
 		slidesPerView: 3,
 		speed: 2000,
 		breakpoints: {
-			1024: {
+			992: {
 				slidesPerView: 2,
 			},
 			576: {
@@ -51,7 +119,7 @@ function roomTypeSlide() {
 		speed: 2000,
 		spaceBetween: 20,
 		breakpoints: {
-			1024: {
+			992: {
 				slidesPerView: 2,
 			},
 			576: {
@@ -60,3 +128,5 @@ function roomTypeSlide() {
 		}
 	})
 }
+
+Header.toggleMenu();
